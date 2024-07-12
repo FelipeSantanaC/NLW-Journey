d6a2323b404cd7a3746574dff5aba2be787e08b3
FelipeSantanaC <felipecabocloo@gmail.com>
2024-07-12 15:15:18 -0300

    feat: implementing dv connection with repository

--- backend/src/models/repositories/trips_repository.py
+++ backend/src/models/repositories/trips_repository.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Tuple
 from sqlite3 import Connection
 
 class TripsRepository:
@@ -11,5 +11,39 @@ class TripsRepository:
             '''
                 INSERT INTO trips
                     (id, destination, start_date, end_date, owner_name, owner_email)
-            '''
+                VALUES
+                    (?, ?, ?, ?, ?, ?)
+            ''',(
+                trips_infos["id"],
+                trips_infos["destination"],
+                trips_infos["start_date"],
+                trips_infos["end_date"],
+                trips_infos["owner_name"],
+                trips_infos["owner_email"],
+            )
         )
+        self.__conn.commit()
+    
+    def find_trip_by_id(self, trip_id: str) -> Tuple:
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            '''
+                SELECT * FROM trips WHERE id = ?
+            ''', (
+                trip_id,
+            )
+        )
+        trip = cursor.fetchone()
+        return trip
+
+    def update_trip_status(self, trip_id: str) -> None:
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            '''
+                UPDATE trips 
+                    SET status = 1
+                WHERE 
+                    id = ?
+            ''',(trip_id,)
+        )
+        self.__conn.commit()
